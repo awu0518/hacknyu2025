985d90c27a7daa291723f0394aed97ed12d6a124
Alexander Wu <wualexander518@gmail.com>
2025-02-08 15:16:47 -0500

    new function for news

--- llm.py
+++ llm.py
@@ -7,12 +7,28 @@ load_dotenv(".env")
 OPENAI_API_KEY = os.environ.get("OPENAI")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-def getMessage(msg):
+def getMessage(stock_info, RSI, SMA, news):
     completion = client.chat.completions.create(
         model="chatgpt-4o-latest",
         messages = [
-            {"role": "developer", "content": "You want to explain why a certain stock goes up and down given certain data"},
-            {"role": "user", "content": msg}
+            {"role": "user", 
+             "content": f"""Given this csv about a certain companies stock {stock_info}, RSI {RSI}, SMA {SMA},
+                        and provided news articles {news}. Only look at the values from the month before the most 
+                        recent date from the csv. Explain why the stock price is going up or down in the next month.
+             """}
+        ]
+    )
+
+    msg = completion.choices[0].message.content
+    print(msg)
+    return msg
+
+def getNews(stock_name):
+    completion = client.chat.completions.create(
+        model="chatgpt-4o-latest",
+        messages = [
+            {"role": "user", 
+             "content": f"Get news articles about {stock_name} from the month of January and provide a brief summary of them."}
         ]
     )
 
